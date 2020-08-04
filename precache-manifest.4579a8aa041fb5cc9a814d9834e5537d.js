1321ee16949e03e0d3b6703d6a1ba3d361da57a9
Eishta <eishtamital@gmail.com>
2020-08-04 19:38:15 +0530

    Updates

---
+++ precache-manifest.4579a8aa041fb5cc9a814d9834e5537d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124da0ed0bc967ac3aab90538601692e",
+    "url": "/triviaQuizo/index.html"
+  },
+  {
+    "revision": "f02c3812930ad67fcdbf",
+    "url": "/triviaQuizo/static/css/main.cd4ad120.chunk.css"
+  },
+  {
+    "revision": "dcd183f29c9b7a4e937d",
+    "url": "/triviaQuizo/static/js/2.e5f1281a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/triviaQuizo/static/js/2.e5f1281a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02c3812930ad67fcdbf",
+    "url": "/triviaQuizo/static/js/main.4f3091cb.chunk.js"
+  },
+  {
+    "revision": "09c1c2486a43584c14c3",
+    "url": "/triviaQuizo/static/js/runtime-main.4f57944c.js"
+  }
+]);
